13db58d94507848a728cbfd00541d8d52fea7cd9
Kravets Roman <roman_kravets_den@mail.ru>
2024-11-11 02:17:24 +0300

    first service + frontend fixed

---
+++ person-microservice/src/main/java/ru/lab3/person_microservice/model/Color.java
@@ -0,0 +1,9 @@
+package ru.lab3.person_microservice.model;
+
+public enum Color {
+    GREEN,
+    BLACK,
+    BLUE,
+    ORANGE,
+    BROWN;
+}
---
+++ person-microservice/src/main/java/ru/lab3/person_microservice/model/Nationality.java
@@ -0,0 +1,9 @@
+package ru.lab3.person_microservice.model;
+
+public enum Nationality {
+    RUSSIAN,
+    CHINESE,
+    AMERICAN,
+    JAPANESE,
+    GERMAN;
+}
