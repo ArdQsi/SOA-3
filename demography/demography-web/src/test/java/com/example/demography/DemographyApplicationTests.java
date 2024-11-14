c748edaea6b8265c40c55ffe6ed3b54c94253e0f
Denstep <denvvbaranov@gmail.com>
2024-11-15 01:41:55 +0300

    add demography

---
+++ demography/demography-web/src/test/java/com/example/demography/DemographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
